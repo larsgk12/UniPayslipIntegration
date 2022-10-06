05719910d38619485be68e2c091d759c5d20400b
Lars Kristiansen <larsgk12@gmail.com>
2022-10-06 14:10:24 +0200

    feature(settings): Added settings to project
    Moved Softrig

---
+++ UniPayslipIntegration/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniPayslipIntegration
+{
+    public class SoftRigSettings
+    {
+        public string authUrl { get; set; }
+        public string softrigUrl { get; set; }
+        public string certificatePassword { get; set; }
+
+    }
+    public class SupabaseSettings
+    {
+        public string url { get; set; }
+        public string key { get; set; }
+
+    }
+}
